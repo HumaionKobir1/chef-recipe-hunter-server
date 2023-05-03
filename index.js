f9950104e428fc0d6b773704574672816505717d
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-04 00:30:03 +0600

    Create chefs data loading API

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const categories = require('./data/category.json');
+const chefData = require('./data/data.json');
+
 
 app.use(cors())
 
@@ -12,6 +14,16 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefData);
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChefs = chefData.find(c => c.id == id);
+    res.send(selectedChefs);
 })
 
 app.listen(port, ()=> {
