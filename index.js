c848ca509ee80ff8e85ed0621660766c622fe531
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-04 21:56:11 +0600

    Added => vercel config file

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const categories = require('./data/category.json');
-const chefData = require('./data/data.json');
 
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipe.json');
+const categories = require('./data/category.json');
 
 app.use(cors())
 
@@ -12,19 +13,31 @@ app.get('/', (req, res) => {
     res.send('Chef is running')
 });
 
-app.get('/categories', (req, res) => {
+app.get('/chefs', (req, res)=> {
+    res.send(chefs);
+})
+
+app.get('/recipes', (req, res)=> {
+    res.send(recipes);
+})
+
+app.get('/categories', (req, res)=> {
     res.send(categories);
 })
 
-app.get('/chefs', (req, res) => {
-    res.send(chefData);
+app.get('/chefs/:id', (req, res)=> {
+    const id = parseInt(req.params.id);
+    const chefsRecipe = recipes.filter(c => parseInt( c.category_id) === id);
+    res.send(chefsRecipe);
 })
 
-app.get('/chefs/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedChefs = chefData.find(c => c.id == id);
-    res.send(selectedChefs);
+app.get('/recipes/:id', (req, res)=> {
+    const id1 = req.params.id;
+    const selectedRecipes = recipes.filter(r=> r.id == id1);
+    console.log(selectedRecipes)
+    res.send(selectedRecipes);
 })
+
 
 app.listen(port, ()=> {
     console.log(`Chef API IS RUNNING ON PORT: ${port}`);
